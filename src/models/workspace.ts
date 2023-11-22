97abdce19ee2a26b9ad9aa423101eab595b7f884
amitabhsahu <amitabh.sahu0008@gmail.com>
2023-11-22 13:37:52 +0530

    added type of workspace readonly

--- src/models/workspace.ts
+++ src/models/workspace.ts
@@ -3,12 +3,27 @@ export interface editorsInterface {
   email: string;
 }
 
-export interface WorkspaceInterface {
+interface WorkspaceSecrets {
+  clientId: string;
+  clientSecret: string;
+}
+
+interface WorkspaceCommon {
   id: string;
   createdAt: string;
   updatedAt: string;
   creatorId: string;
-  clientId: string;
-  clientSecret: string;
-  editorIds: editorsInterface[];
+  editors: editorsInterface[];
 }
+
+export type WorkspaceInterface = WorkspaceSecrets & WorkspaceCommon;
+
+interface EditorWorkspace extends WorkspaceCommon {
+  role: "editor";
+}
+
+interface CreatorWorkspace extends WorkspaceInterface {
+  role: "creator";
+}
+
+export type WorkspaceReadOnly = EditorWorkspace | CreatorWorkspace;
